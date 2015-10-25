2ca1ddf6c19486243dc5fd23a3ac4b6c6810ae03
Bill Cauchois <wcauchois@gmail.com>
2015-10-25 13:52:26 -0400

    create src/clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pepehub "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [environ "1.0.1"]
                  [ring/ring-jetty-adapter "1.4.0"]
