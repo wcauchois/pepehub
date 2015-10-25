b37783a2568d228d135b90007cf1bc901f5cc21a
Bill Cauchois <wcauchois@gmail.com>
2015-10-25 13:45:38 -0400

    more cargo culting

--- project.clj
+++ project.clj
@@ -8,5 +8,8 @@
                  [compojure "1.4.0"]]
   :uberjar-name "pepehub-standalone.jar"
   :plugins [[lein-environ "1.0.1"]]
+  :hooks [environ.leiningen.hooks]
+  :min-lein-version "2.0.0"
   :main pepehub.core
-  (comment :profiles {:production {:env {:production true}}}))
+  :profiles {:production {:env {:production true}}
+             :uberjar {:aot :all}})
