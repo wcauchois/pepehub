fcdfe827cca2dfb8904da34c12947bcadf21229a
Bill Cauchois <wcauchois@gmail.com>
2015-12-04 22:27:58 -0500

    remove vestiges of flux architecture

--- src/js/BaseWithEvents.js
+++
@@ -1,8 +0,0 @@
-var Base = require('basejs'),
-    _ = require('lodash'),
-    EventEmitter = require('events').EventEmitter;
-
-var BaseWithEvents = Base.extend();
-_.extend(BaseWithEvents.prototype, EventEmitter);
-
-module.exports = BaseWithEvents;
